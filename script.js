3e30845158ec4fb6b8331e82016ac4484727a436
Renato Magrini <renatomagrini@gmail.com>
2021-02-27 18:09:00 +0100

    html and button event listener started

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Taggin elements
+
+document.getElementById("generate").addEventListener("click", function () {
+  let txtNumber = document.getElementById("txtnumber").value;
+
+  let number = Number(txtNumber);
+
+  alert(number);
+});
